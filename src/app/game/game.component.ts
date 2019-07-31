4a2d66fd8373ad4da76dba86876e880f94fd49e4
corneliapelech <ramblingsoliloquy@gmail.com>
2019-07-31 20:33:03 +0200

    clean-up

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -23,7 +23,6 @@ export class QuitGame {
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html'
-  // styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit, OnDestroy {
 
